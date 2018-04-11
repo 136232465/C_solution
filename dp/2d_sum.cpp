911fe4162857ee8b46fd9914bf7b6cc056f35133
guowei <136232465@qq.com>
2018-04-11 11:38:59 +0800

    range sum query 2d

---
+++ dp/2d_sum.cpp
@@ -0,0 +1,24 @@
+//leetcode 304 Range sum query 2D-immutable
+
+
+
+class NumMatrix {
+private:
+    vector<vector<int>> sums;
+public:
+    NumMatrix(vector<vector<int>> matrix) {
+        
+        int row = matrix.size();
+        int col = row == 0 ? 0 : matrix[0].size();
+        sums = vector<vector<int>>(row+1, vector<int>(col+1, 0));
+        for (int i = 1; i <= row; ++i){
+            for(int j = 1; j <= col; ++j){
+                sums[i][j] =  matrix[i-1][j-1] + sums[i-1][j] + sums[i][j-1] - sums[i-1][j-1];
+            }
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return sums[row2+1][col2+1] + sums[row1][col1] - sums[row2+1][col1] - sums[row1][col2+1];
+    }
+};
--- string/greater.cpp
+++ string/greater.cpp
@@ -4,35 +4,113 @@
 
 #include<vector>
 #include<algorithm>
+#include <iostream>
+#include <string>
+#include <queue>
+#include <hash_set>
 using namespace std;
 
-class Solution {
+//class Solution {
+//public:
+//	int maxProduct(int length) {
+//		// write your solution here
+//		vector<int> dp(length + 1, 0);
+//		for (int i = 1; i <= length; ++i){
+//			dp[i] = i-1;
+//			for (int j = 1; j < i; ++j){
+//				dp[i] = max(dp[i], max(dp[i - j],(i-j)) * j);
+//			}
+//		}
+//		return dp[length];
+//	}
+//};
+
+//class Solution {
+//public:
+//	queue<int> lay;
+// 	
+//	string getpath(string s){
+//		int len = s.length();
+//		if (len < 2){
+//			return s;
+//		}
+//		int first = 0;
+//		int second = 1;
+//		while (second != len){
+//			if (s[first] == '/' && s[second] == '/'){
+//				s.erase(s.begin() + second);
+//				--len;
+//			}
+//			else {
+//				++first;
+//				++second;
+//			}
+//		}
+//		if (s[first] == '/'){
+//			s.erase(s.begin() + first);
+//		}
+//		return s;
+//	}
+//};
+//
+//class Solution{
+//	public:
+//		string trainstr(string s){
+//			if (s.empty()){
+//				return NULL;
+//			}
+//			int i = 0, j = 0;
+//			for (; i < s.size();){
+//				if (s[i] >= '0' && s[i] <= '9'){
+//					i += s[i] - '0' +1;
+//					s[j] = s[i];
+//
+//				} else {
+//					++i; ++j;
+//				}
+//			}
+//			return s.substr(0, j);
+//		}
+//		//这是一个句子
+//};
+
+string words[12] = { "我", "要", "我要", "找", "嘉兴", "的", "麦当劳", "麦", "劳", "的麦", "当劳", "我要找嘉兴的麦当劳" };
+hash_set<string> dict(words, words + 12);
+string sentence;
+
+
+class Solution{
 public:
-	int maximalSquare(vector<vector<char>>& matrix) {
-		vector<vector<int>> dp;
-		for (int i = 0; i < matrix.size(); ++i){
-			vector<int> dpi;
-			for (int j = 0; j < matrix[i].size(); ++j){
-				int num = matrix[i][j] - '0';
-				dpi.push_back(num);
-			}
-			dp.push_back(dpi);
+	void trainstr(string s, vector<vector<string>>& res, vector<string>& cursentence){
+		if (s.empty()){
+			res.push_back(cursentence);
 		}
-		int maxres = 0;
-		for (int i = 0; i < dp.size(); ++i){
-			for (int j = 0; j < dp[i].size(); ++j){
-				if (i != 0 && j != 0 && dp[i][j] != 0){
-					dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1;
-				}
-				maxres = max(dp[i][j], maxres);
+		//string sentence = "我要找嘉兴的麦当劳";
+		string temp;
+		for (int i = 0; i < s.size(); ++i){
+			temp += s[i];
+			++i;
+			temp += s[i];
+			if (dict.find(temp) != dict.end()){
+				cursentence.push_back(temp);
+				string remain(s.begin()+i+1,s.end());
+				trainstr(remain, res, cursentence);
+				cursentence.pop_back();
 			}
-		}
-		return maxres*maxres;
+		}		
 	}
+
 };
 
-void main(){
-	vector<vector<char>> src(2, vector<char>(4,'1'));
-	Solution s;
-	s.maximalSquare(src);
+
+int main(){
+	string sentence = "我要找嘉兴的麦当劳";
+	string s ;
+	Solution obj;
+	vector<vector<string>> res;
+	vector<string> curcomb;
+	obj.trainstr(sentence,res,curcomb);
+	//s += sentence[0]+sentence[1];
+	//cout << s << endl;
+	system("pause");
 }
