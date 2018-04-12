5fc33fd48980cc1dfb703a5087225094d1827ed0
guowei <136232465@qq.com>
2018-04-12 14:37:41 +0800

    target_sum

---
+++ dp/targetsum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        if (sum < S || (sum+S)%2 != 0){
+            return 0;
+        }
+        int target = (sum+S)/2;
+        int dp[target+1] = {0};
+        dp[0] = 1;
+        for (auto n : nums){
+            for (int i = target; i >= n; --i){
+                dp[i] += dp[i-n];
+            }
+        }
+        return dp[target];        
+    }
+};
